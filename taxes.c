78c68b128a031bb9cca5a0944b0e0ff976f51c84
syeda khoula abid <syeda.khoula.abid234@gmail.com>
2023-10-22 23:22:38 +0500

    taxes program

--- taxes.c
+++ taxes.c
@@ -29,6 +29,24 @@ int main(int argc, char **argv) {
   }
 
   //TODO: compute the tax, child credit, and total tax here
+    if(agi>=0 && agi<=100000)
+    {
+      tax = agi * 0.10;
+      printf("%f", tax);
+    }
+    else if(agi>50000 && agi<=100000)
+    {
+      tax=5000 + (agi-50000) * 0.20;
+      printf("%f",tax);
+    }
+    else 
+    {
+      printf("%f", tax);
+    }
+
+    childCredit = numChildren * 1000;
+    totalTax = childCredit - tax;
+
 
   printf("AGI:          $%10.2f\n", agi);
   printf("Tax:          $%10.2f\n", tax);
